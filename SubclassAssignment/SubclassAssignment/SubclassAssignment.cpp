1abe777825a170f86e434f5e45ebad31baadbb03
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-12-12 20:49:24 -0700

    Fixed mistakes

--- SubclassAssignment/SubclassAssignment/SubclassAssignment.cpp
+++ SubclassAssignment/SubclassAssignment/SubclassAssignment.cpp
@@ -10,18 +10,12 @@ class Shape
 public:
     string color;
 
-    double getArea(double height, double width)
+    double getArea()
     {
-        return height * width;
+
     }
-    double getArea(double base, double height)
-    {
-        return 0.5 * base * height;
-    }
-    double getArea(double radius)
-    {
-        return 3.14 * radius;
-    }
+    
+    
 };
 
 class Rectangle : public Shape
@@ -29,17 +23,32 @@ class Rectangle : public Shape
 public:
     double height;
     double width;
+
+    double getArea(double height, double width)
+    {
+        return height * width;
+    }
 };
 class Triangle : public Shape
 {
 public:
     double base;
     double height;
+
+    double getArea(double base, double height)
+    {
+        return 0.5 * base * height;
+    }
 };
 class Circle : public Shape
 {
 public:
     double radius;
+
+    double getArea(double radius)
+    {
+        return 3.14 * radius;
+    }
 };
 
 
